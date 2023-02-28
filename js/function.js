fe2a033f043e14c9f09c67e72aaec4de85d9d2be
LucianaSacaba <126592309+LucianaSacaba@users.noreply.github.com>
2023-02-28 20:17:24 -0300

    Primer commit

---
+++ js/function.js
@@ -0,0 +1,7 @@
+
+document.getElementById('completar').addEventListener('click', function (){
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    document.getElementById("fondo").src = "imagenes/personas-2.png";
+})
+
